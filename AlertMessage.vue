c1d0385f226e51c71f11229bba4c3d47be6bc460
sebastien <heligon.seb@gmail.com>
2022-06-07 10:48:13 +0200

    ajout des component alertMessage et spinner

---
+++ AlertMessage.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="alert text" :class="classList()" role="alert">
+        <i v-if="icon" class="m-2 bi" :class="icon"></i>
+        
+        <slot></slot>
+
+        <button v-if="dismissible" type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+    </div>
+</template>
+
+<script>
+
+export default {
+    props: {
+        variant: {
+            type: String,
+            default: "info"
+        },
+        dismissible: {
+            type : Boolean,
+            default: false
+        },
+        icon: {
+            type: String,
+            default: ""
+        }
+    },
+
+    methods: {
+        /**
+         * Ajout une liste de class
+         * 
+         * @return {String}
+         */
+        classList() {
+            let classStringList="";
+
+            if(this.variant) {
+                classStringList += ' alert-'+ this.variant;
+            }
+
+            if(this.dismissible) {
+                classStringList += ' alert-dismissible'
+            }
+
+            return classStringList;
+        }
+    },
+}
+
+</script>
