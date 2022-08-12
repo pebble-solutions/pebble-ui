d16c5f6f5573078a4fd14075ca696d4fb43b353f
sebastien <heligon.seb@gmail.com>
2022-08-12 14:58:42 +0200

    rework progressbar in to a charts/stackedBar. code upgrade / add % on the bar

---
+++ charts/StackedBar.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="progress my-2">
+        <div v-for="(bar, index) in bars" :key="'bar'+ index" 
+            class="progress-bar" :class="'bg-'+bar.color"
+            role="progressbar"
+            aria-valuemax="100" aria-valuemin="0" :aria-valuenow="bar.value"
+            :style="'width:'+bar.value+'%'">
+            <span v-if="percentage">{{bar.value}}%</span>
+        </div>
+    </div>
+</template>
+
+<script>
+/**
+ *  props :
+ * - bar {Array} :
+ * {
+ *      color:          Couleur de boostrap 
+ *      value:          Valeur en % de la bar
+ * }
+ *        
+ */
+export default {
+    props: {
+        bars: Array,
+        percentage: {
+            type: Boolean,
+            default: false
+        },
+    },
+}
+</script>
