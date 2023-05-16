51439af179880298d400eb169871043a0b402d4e
Guillaume MODARD <guillaumemodard@gmail.com>
2023-05-16 15:25:07 +0200

    Ajout du progress awaiting

---
+++ loader/ProgressAwaiting.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="progress" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" :style="pxWidth">
+        <div class="progress-bar progress-bar-striped progress-bar-animated" :class="progressClass" style="width: 100%"></div>
+    </div>
+</template>
+
+<script>
+
+export default {
+    props: {
+        size: Number,
+        progressClass: {
+            type: String,
+            default: 'bg-secondary opacity-25'
+        }
+    },
+
+    computed: {
+        /**
+         * Retourne la largeur de l'élément en px
+         * 
+         * @return {string}
+         */
+        pxWidth() {
+            return this.size ? `width:${this.size}px;` : '';
+        }
+    }
+}
+
+</script>
